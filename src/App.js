13f251601a76905f12df2b765be1d4c212c01a84
Nick Pruitt <nick@abenity.com>
2017-05-22 09:43:10 -0500

    First Commit!
    Yeah, I’m making this a React app for no reason other than learning something new. But learning some new is cool, right?! Anyone? …

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import './App.css';
+import nameLetters from './nameLetters'; // the letters data that builds my name
+
+function changeFontWeight(state, props){
+  if ( state.fontWeight === 'thin' ) {
+    return { fontWeight: 'light' }
+  }
+
+  if ( state.fontWeight === 'light' ) {
+    return { fontWeight: 'medium' }
+  }
+
+  if ( state.fontWeight === 'medium' ) {
+    return { fontWeight: 'bold' }
+  }
+
+  if ( state.fontWeight === 'bold' ) {
+    return { fontWeight: 'black' }
+  }
+
+  if ( state.fontWeight === 'black' ) {
+    return { fontWeight: 'thin' }
+  }
+}
+
+class LetterItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { fontWeight: 'thin' };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(changeFontWeight);
+  }
+
+  render() {
+    return <span onClick={this.handleClick} className={this.state.fontWeight}>{this.props.value.letter}</span>;
+  };
+}
+
+class LetterList extends Component {
+  render() {
+    const letters = this.props.letters;
+
+    const letterItems = letters.map((letter) =>
+      <div key={letter.id} className={letter.class}>
+        <LetterItem value={letter} />
+      </div>
+    );
+
+    return(
+      <div className="grid">
+        {letterItems}
+      </div>
+    );
+  }
+
+}
+
+class Letters extends Component {
+  render() {
+    return (
+      <LetterList letters={this.props.letters} />
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <Letters letters={nameLetters} />
+      </main>
+    );
+  }
+}
+
+export default App;
---
+++ src/nameLetters.js
@@ -0,0 +1,54 @@
+const nameLetters = [
+  {
+    id: 1,
+    letter: 'N',
+    class: 'N'
+  },
+  {
+    id: 2,
+    letter: 'I',
+    class: 'I'
+  },
+  {
+    id: 3,
+    letter: 'C',
+    class: 'C'
+  },
+  {
+    id: 4,
+    letter: 'K',
+    class: 'K'
+  },
+  {
+    id: 5,
+    letter: 'P',
+    class: ' P'
+  },
+  {
+    id: 6,
+    letter: 'R',
+    class: ' R'
+  },
+  {
+    id: 7,
+    letter: 'U',
+    class: ' U'
+  },
+  {
+    id: 8,
+    letter: 'I',
+    class: ' I-2'
+  },
+  {
+    id: 9,
+    letter: 'T',
+    class: ' T'
+  },
+  {
+    id: 10,
+    letter: 'T',
+    class: ' T-2'
+  }
+];
+
+export default nameLetters;
